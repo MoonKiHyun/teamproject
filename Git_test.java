6d40e17dc5e248298fae9016b2350b7cd7f6c68b
rinyejo <rinyejoin@gmail.com>
2023-10-25 12:30:32 +0900

    conflict test

--- Git_test.java
+++ Git_test.java
@@ -3,9 +3,11 @@ package sparta.teamproject;
 public class Git_test {
 
     public static void main(String[] args) {
-        System.out.println("moon 테스트");
-        System.out.println("hanjoon 테스트");
-        System.out.println("rinyejo 테스트");
-        System.out.println("rinyejo 테스트2");
+
+
+
+
+        System.out.println("conflict test");
+
     }
 }
