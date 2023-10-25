8159a3d4a11b5660ad188a1a05ca17531eb4664d
rinyejo <rinyejoin@gmail.com>
2023-10-25 12:12:05 +0900

    rinye test

--- Git_test.java
+++ Git_test.java
@@ -5,5 +5,6 @@ public class Git_test {
     public static void main(String[] args) {
         System.out.println("moon 테스트");
         System.out.println("hanjoon 테스트");
+        System.out.println("rinyejo 테스트");
     }
 }
